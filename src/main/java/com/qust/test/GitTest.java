209b5b18c3a2f1eeb0bad1366bc8f83deeda3147
Yaojl <903337655@qq.com>
2022-04-19 15:24:59 +0800

    hot-fix-next

--- src/main/java/com/qust/test/GitTest.java
+++ src/main/java/com/qust/test/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git hot fix");
+        System.out.println("hello git hot fix test");
     }
 }
